1ea960f177410f039d02fe96bdcf8d87f12a6707
Mitch4sho <mitchelloliver1991@gmail.com>
2020-10-01 11:00:14 -0700

    added proprty functions to the UI Controller so the Global Controller may have access to those proproties

--- script.js
+++ script.js
@@ -5,7 +5,29 @@ var budgetController = (function () {
 
 //UI CONTROLLER
 var UIController = (function () {
+    // This is where you will search for the data 
+    var DOMstrings = {
+        inputType: '.addType',
+        inputDescriptions: '.description',
+        inputValue: '.value',
+        inputBtn: '.addBtn'
+    }
 
+    return {
+        getInput: function () {
+            return {
+                // returning a object so you have access to the properties instead of variables
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp 
+                description: document.querySelector(DOMstrings.inputDescriptions).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        //this DOMstrings is now public 
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+
+    };
 
 
 })();
@@ -13,9 +35,12 @@ var UIController = (function () {
 //GLOBAL CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings(); // DON'T FORGET TO CALL THE FUNCTION 
 
     var ctrlAddItem = function () {
-        // 1. Get input Data  
+        // 1. Get input Data
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // 2. add the item to the budget controller 
 
@@ -28,7 +53,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     }
 
-    document.querySelector('.addBtn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
         if (event.keycode === 13 || event.which === 13) {
